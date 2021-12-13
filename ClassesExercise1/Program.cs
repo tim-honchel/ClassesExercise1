7ce1b0c6eeb6569623c7ae5d1eaafa4f2e13ceb0
Tim Honchel <tim.honchel@gmail.com>
2021-12-13 15:01:46 -0500

    first commit

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car("Honda", "Insight", 2021);
+            Console.WriteLine(car1.Make + " " + car1.Model + " " + Convert.ToString(car1.Year));
+        }
+    }
+}
